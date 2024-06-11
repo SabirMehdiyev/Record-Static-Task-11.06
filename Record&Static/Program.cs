4435e5e36eae41ff924cb24da04ca196f81b26a2
SabirMehdiyev <sabirsm@code.edu.az>
2024-06-11 17:21:41 +0400

    Add project files.

---
+++ Record&Static/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Record_Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[3];
+            int userCount = 0;
+            while (userCount < users.Length)
+            {
+                Console.WriteLine("Zehmet olmasa email daxil edin:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Zehmet olmasa şifrəni daxil edin:");
+                string password = Console.ReadLine();
+
+                users[userCount] = new User(email, password);
+                userCount++;
+                Console.WriteLine("Istifadeci elave olundu");
+            }
+            while (true)
+            {
+                Console.WriteLine
+                    ("1.Show all users \n" +
+                     "2.Get info by id\n" +
+                     "0.Quit");
+
+                int command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        Console.WriteLine("All users:");
+                        for (int i = 0; i < userCount; i++)
+                        {
+                            users[i].GetInfo();
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Id daxil edin");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        int index = User.FindByID(users, id);
+                        if (index != -1)
+                        {
+                            users[index].GetInfo();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bele istifadeci movcud deyil");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Zehmet olmasa duzgun komanda daxil edin");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Record&Static/User.cs
@@ -0,0 +1,76 @@
+﻿namespace Record_Static;
+
+internal class User
+{
+    private static int id;
+    private string _password;
+    public int Id { get; }
+    public string Fullname { get; set; }
+    public string Email { get; set; }
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            while (!PasswordChecker(value))
+            {
+                Console.WriteLine("Sifreni duzgun daxil edin:");
+                value = Console.ReadLine();
+            }
+            _password = value;
+        }
+    }
+    public User(string email, string password)
+    {
+        id++;
+        Id = id;
+        Email = email;
+        Password = password;
+
+    }
+
+    public bool PasswordChecker(string password)
+    {
+        bool hasUpper = false;
+        bool hasLower = false;
+        bool hasDigit = false;
+        if (password.Length >= 8)
+        {
+            foreach (var item in password)
+            {
+                if (char.IsUpper(item))
+                {
+                    hasUpper = true;
+                }
+                else if (char.IsLower(item))
+                {
+                    hasLower = true;
+                }
+                else if (char.IsDigit(item))
+                {
+                    hasDigit = true;
+                }
+                if (hasUpper && hasLower && hasDigit)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine($"Id:{Id} - Fullname:{Fullname} - Email {Email}");
+    }
+    public static int FindByID(User[] users, int id)
+    {
+        for (int i = 0; i < users.Length; i++)
+        {
+            if  (users[i].Id == id)
+            {
+                return i; 
+            }
+        }
+        return -1; 
+    }
+}
